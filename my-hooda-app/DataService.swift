8a2d46110343c1a8c097f8eaa672a1ca3cb76103
cdolm92 <cdolm92@gmail.com>
2016-02-23 04:19:14 -0500

    everything saves

---
+++ my-hooda-app/DataService.swift
@@ -0,0 +1,69 @@
+//
+//  DataService.swift
+//  my-hooda-app
+//
+//  Created by Christella on 2/23/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//This is a singleton-- will live in memory for the lifetime of the application
+
+class DataService {
+    static let instance = DataService()
+    
+    let KEY_POSTS = "posts"
+    private var _loadedPosts = [Post]()
+    
+    var loadedPosts: [Post] {
+        return _loadedPosts
+    }
+    
+    func savePosts() {
+        let postData = NSKeyedArchiver.archivedDataWithRootObject(_loadedPosts)
+        NSUserDefaults.standardUserDefaults().setObject(postData, forKey: KEY_POSTS)
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    
+    func loadPosts() {
+       if let postData = NSUserDefaults.standardUserDefaults().objectForKey(KEY_POSTS) as? NSData {
+        
+        if let postsArray = NSKeyedUnarchiver.unarchiveObjectWithData(postData) as? [Post] {
+            _loadedPosts = postsArray
+            }
+        }
+        
+        NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "postsLoaded", object: nil))
+    }
+    
+    func saveImageAndCreatePath(image: UIImage) -> String {
+        let imgData = UIImagePNGRepresentation(image)
+        let imgPath = "image\(NSDate.timeIntervalSinceReferenceDate()).png"
+        let fullPath = documentsPathForFileName(imgPath)
+        imgData?.writeToFile(fullPath, atomically: true)
+        
+        return imgPath
+    }
+    
+    func imageForPath(path: String) -> UIImage? {
+        let fullPath = documentsPathForFileName(path)
+        let image = UIImage(named: fullPath)
+        
+        return image
+    }
+    
+    func addPost(post: Post) {
+        _loadedPosts.append(post)
+        savePosts()
+        loadPosts()
+    }
+    
+    func documentsPathForFileName(name: String) -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentationDirectory, .UserDomainMask, true)
+        let fullPath = paths[0] as NSString
+        return fullPath.stringByAppendingPathComponent(name)
+    }
+    
+}
