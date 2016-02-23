41e5496fc1d971714de308fda38654f27c9c7f08
cdolm92 <cdolm92@gmail.com>
2016-02-23 02:18:33 -0500

    added addpostvc

---
+++ my-hooda-app/AddPostVC.swift
@@ -0,0 +1,20 @@
+//
+//  AddPostVC.swift
+//  my-hooda-app
+//
+//  Created by Christella on 2/23/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
