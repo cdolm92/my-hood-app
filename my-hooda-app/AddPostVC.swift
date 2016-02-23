783b42faff86c50184e35139c8cba9ba6d941cba
cdolm92 <cdolm92@gmail.com>
2016-02-23 02:37:41 -0500

    outlets in addVC were added to the controller

--- my-hooda-app/AddPostVC.swift
+++ my-hooda-app/AddPostVC.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class AddPostVC: UIViewController {
+    
+    
+    @IBOutlet weak var descField: UITextField!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var postImg: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        postImg.layer.cornerRadius = postImg.frame.size.width / 2
+        postImg.clipsToBounds = true
     }
 
+    @IBAction func addPicBtnPressed(sender: UIButton) {
+        sender.setTitle("", forState: .Normal)
+    }
+    
+    @IBAction func makePostBtnPressed(sender: AnyObject) {
+        
+    }
 
+    @IBAction func cancelBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
