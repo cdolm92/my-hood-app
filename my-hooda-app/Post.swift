ea8487bca18c8c729c9036019f92144f6075d8ac
cdolm92 <cdolm92@gmail.com>
2016-02-23 00:35:14 -0500

    added post class

---
+++ my-hooda-app/Post.swift
@@ -0,0 +1,23 @@
+//
+//  Post.swift
+//  my-hooda-app
+//
+//  Created by Christella on 2/23/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+import Foundation
+
+class post {
+    
+    private var imagePath: String
+    private var title: String
+    private var postDesc: String
+    
+    init(imagePath: String, title: String, description: String) {
+        self.imagePath = imagePath
+        self.title = title
+        self.postDesc = description
+    }
+    
+}
